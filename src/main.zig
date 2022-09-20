55f9b5b4b8b56fc54222a218c15e62ec947e895a
Vincent Rischmann <vincent@rischmann.fr>
2022-09-20 21:49:08 +0200

    add a flag to fetch all towns

--- src/main.zig
+++ src/main.zig
@@ -28,7 +28,7 @@ pub fn main() anyerror!void {
 
     //
 
-    var departement_code: ?[]const u8 = null;
+    var fetch_all_towns: bool = false;
 
     var raw_args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, raw_args);
@@ -37,13 +37,7 @@ pub fn main() anyerror!void {
     while (i < raw_args.len) : (i += 1) {
         const arg = raw_args[i];
 
-        if (mem.startsWith(u8, arg, "--departement-code=")) {
-            const pos = mem.indexOfScalar(u8, arg, '=').?;
-            departement_code = arg[pos + 1 ..];
-        } else if (mem.eql(u8, arg, "--departement-code")) {
-            i += 1;
-            departement_code = raw_args[i];
-        }
+        if (mem.eql(u8, arg, "--fetch-all-towns")) fetch_all_towns = true;
     }
 
     //
@@ -69,18 +63,20 @@ pub fn main() anyerror!void {
     try db.exec("CREATE VIRTUAL TABLE decoupage_administratif USING apida", .{ .diags = &diags }, .{});
     try db.exec("CREATE VIRTUAL TABLE user USING user(host=localhost, port=6379)", .{ .diags = &diags }, .{});
 
-    //
+    var row_arena = std.heap.ArenaAllocator.init(allocator);
+    defer row_arena.deinit();
 
-    var n: usize = 0;
-    while (n < 2) : (n += 1) {
-        var row_arena = std.heap.ArenaAllocator.init(allocator);
-        defer row_arena.deinit();
+    const start = std.time.milliTimestamp();
+
+    if (fetch_all_towns) {
+        defer {
+            const now = std.time.milliTimestamp();
+            logger.debug("fetched all towns in {d}ms", .{now - start});
+        }
 
         var stmt = try db.prepareWithDiags(
-            \\SELECT u.rowid, u.id, u.name, u.postal_code, (
-            \\  SELECT group_concat(da.town) FROM decoupage_administratif da WHERE da.postal_code = u.postal_code
-            \\) AS town
-            \\FROM user u
+            \\SELECT da.town, da.population, da.postal_code
+            \\FROM decoupage_administratif da
         ,
             .{ .diags = &diags },
         );
@@ -88,28 +84,67 @@ pub fn main() anyerror!void {
 
         var iter = try stmt.iterator(
             struct {
-                rowid: i64,
-                id: []const u8,
-                name: []const u8,
-                postal_code: f64,
                 town: []const u8,
+                population: i64,
+                postal_code: i64,
             },
             .{},
         );
 
         var count: usize = 0;
         while (try iter.nextAlloc(row_arena.allocator(), .{ .diags = &diags })) |row| {
-            logger.info("n#{d} row rowid={d} id=\"{s}\" name=\"{s}\": postal code: {d} town: \"{s}\"", .{
-                n,
-                row.rowid,
-                std.fmt.fmtSliceEscapeLower(row.id),
-                row.name,
-                row.postal_code,
+            logger.info("town=\"{s}\" population={d} postal code={d}", .{
                 row.town,
+                row.population,
+                row.postal_code,
             });
             count += 1;
         }
 
-        logger.info("n#{d} count: {d}", .{ n, count });
+        logger.info("count: {d}", .{count});
+    } else {
+        defer {
+            const now = std.time.milliTimestamp();
+            logger.debug("joined tables in {d}ms", .{now - start});
+        }
+
+        var n: usize = 0;
+        while (n < 2) : (n += 1) {
+            var stmt = try db.prepareWithDiags(
+                \\SELECT u.rowid, u.id, u.name, u.postal_code, (
+                \\  SELECT group_concat(da.town) FROM decoupage_administratif da WHERE da.postal_code = u.postal_code
+                \\) AS town
+                \\FROM user u
+            ,
+                .{ .diags = &diags },
+            );
+            defer stmt.deinit();
+
+            var iter = try stmt.iterator(
+                struct {
+                    rowid: i64,
+                    id: []const u8,
+                    name: []const u8,
+                    postal_code: i64,
+                    town: []const u8,
+                },
+                .{},
+            );
+
+            var count: usize = 0;
+            while (try iter.nextAlloc(row_arena.allocator(), .{ .diags = &diags })) |row| {
+                logger.info("n#{d} row rowid={d} id=\"{s}\" name=\"{s}\": postal code: {d} town: \"{s}\"", .{
+                    n,
+                    row.rowid,
+                    std.fmt.fmtSliceEscapeLower(row.id),
+                    row.name,
+                    row.postal_code,
+                    row.town,
+                });
+                count += 1;
+            }
+
+            logger.info("n#{d} count: {d}", .{ n, count });
+        }
     }
 }
